3072482e5d6ac1c55e4854e4f64a1b07da229e2f
Guilherme Utzig <guilherme@digitalrepublic.com.br>
2020-06-10 17:23:23 -0300

    Started frontend project

--- restaurant/frontend/lib/main.dart
+++ restaurant/frontend/lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text(
-        'Hello, World!',
-        textDirection: TextDirection.ltr,
-      ),
+    Container(
+      decoration: new BoxDecoration(color: Colors.red),
+      child: Center(
+        child: Text(
+          'Hello, World!',
+          textDirection: TextDirection.ltr,
+        ),
+      )
     )
   );
 }
